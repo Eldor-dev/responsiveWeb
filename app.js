c3eda97a4292aac87cfc4ecfbd11171bf6dad6e3
Eldor Azimov <eldorazimov37@gmail.com>
2021-07-08 21:13:34 +0900

    added app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const menu = document.querySelector('#mobile-menu');
-const menuLists = document.querySelector('.navbar__menu');
+const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
-    menuLists.classList.toggle('active');
+    menuLinks.classList.toggle('active');
 };
 
 menu.addEventListener('click', mobileMenu);
@@ -43,3 +43,16 @@ const highlightMenu = () => {
  
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
+
+// Close mobile Menu when clicking on menu item
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }   
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
